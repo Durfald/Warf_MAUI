93d66aa7468db76abcfed6454cecf1afc2e5d0bd
Mind-Glitch <rosst0564@gmail.com>
2025-06-09 17:11:11 +0000

    UI_Clean

---
+++ Warf_MAUI/Warf_MAUI.Shared/wwwroot/scripts/typescript/decorations/navigation-button-dynamic-underline.ts
@@ -0,0 +1,38 @@
+﻿const decorationClassName = 'wa-interactive-underline';
+
+const navigationItem = document
+    .getElementsByTagName('nav').item(0);
+console.log(navigationItem);
+
+if (navigationItem) {
+    var decoration_underline: HTMLCollectionOf<HTMLDivElement> | null =
+        navigationItem.getElementsByClassName(decorationClassName) as HTMLCollectionOf<HTMLDivElement>;
+    console.log(decoration_underline);
+    if (decoration_underline == null || decoration_underline == undefined)
+        throw new Error(`Cannot find elments with class '${decorationClassName}'`);
+
+    for (var index = 0; index < decoration_underline.length; index++) {
+        let decorationElement = decoration_underline[index] as HTMLDivElement;
+        if (!decorationElement)
+            throw new Error("decorationElement was null in navigation decoration" +
+                "underline elements list.");
+
+        let decorationParent = decoration_underline[index].parentElement as HTMLElement;
+        if (!decorationParent)
+            throw new Error("decorationParent was null for decoration underline element.");
+
+        decorationParent.addEventListener('mouseover',
+            function (ev) { ShowDecoration(decorationElement, ev, this) });
+
+        decorationParent.addEventListener('mouseleave',
+            function (ev) { HideDecoration(decorationElement, ev, this) });
+    }
+}
+
+function ShowDecoration(decorationElement: HTMLElement, ev: MouseEvent, linkElement: HTMLElement) {
+    decorationElement.style.width = `${linkElement.clientWidth}px`;
+}
+
+function HideDecoration(decorationElement: HTMLElement, ev: MouseEvent, linkElement: HTMLElement) {
+    decorationElement.style.width = `0px`;
+}
